d3e4d04d9580ff2b6dd4a69ca93b30fcbe934f70
Manish Kumar <manishhansalmeh@gmail.com>
2022-04-25 22:08:12 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userModel = require('./user');
 const savingsAccountModel = require('./savingsAccount');
+const fixedAccountModel = require('./fixedAccount');
 
 app.use(express.json());
 
@@ -27,6 +28,33 @@ app.post('/createSavingsAccount', async (req, res, next) => {
         console.log('req.body', req.body)
         let savingsAccountDetail = req.body;
         let response = await savingsAccountModel.insertMany([savingsAccountDetail]);
+        res.json(response);
+        
+    }
+    catch(error) {
+        res.json(error)
+    }
+})
+
+
+app.get('/users', async (req, res) => {
+    try {
+        
+        let response = await userModel.find({});
+        res.json(response);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+app.post('/createFixedAccount', async (req, res, next) => {
+    //fetch info from request body.
+    try {
+
+        console.log('req.body', req.body)
+        let fixedAccountDetail = req.body;
+        let response = await fixedAccountModel.insertMany([fixedAccountDetail]);
         res.json(response);
         
     }
